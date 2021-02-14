4b430d88d3b713b764ed0ca07ca6116ac9faa368
Kyler Harrison <harri828@purdue.edu>
2021-02-13 19:39:05 -0500

    added some json functions (I think)

--- utils.py
+++ utils.py
@@ -85,6 +85,13 @@ def load_dict_from_json(path):
         return json.load(f)
 
 
+# need form of [{word: {title: n, ..., titleN: n}, {...}, {wordN: {title: n, ..., titleN: n}}}]
+def json_to_json_arr(master_path, out_path):
+    master_dict = load_dict_from_json(master_path)
+    json_arr = [{"word_key": word_key, "title_vals": word_vals} for word_key, word_vals in master_dict.items()]
+    write_dict_to_json(json_arr, out_path)
+
+
 # desc.: upload file to s3 bucket
 # input: s3_resource, initialized boto3 thing
 #        file_path, local path to the file to upload
